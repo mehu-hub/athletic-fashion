6e63fb7e079d205cc24819456d5981b14ae3889e
Mehedi Hasan <designwithmehedi@gmail.com>
2023-12-08 11:56:26 +0600

    New Collection and Newsletter

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import Hero from '../Components/Hero/Hero';
 import Popular from '../Components/Popular/Popular';
 import Offer from '../Components/Offer/Offer';
+import NewCollection from '../Components/NewCollection/NewCollection';
+import NewsLetter from '../Components/NewsLetter/NewsLetter';
 
 const Shop = () => {
     return (
         <div>
             <Hero />
             <Popular />
-            <Offer/>
+            <Offer />
+            <NewCollection />
+            <NewsLetter/>
         </div>
     );
 };
